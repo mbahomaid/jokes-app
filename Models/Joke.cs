439224610047dd5e7bb49db4165da5505ce06b04
Bahomaid (Takamura) <bahomaid17@proton.me>
2025-07-06 18:30:47 +0300

    Create Joke Model Class

---
+++ Models/Joke.cs
@@ -0,0 +1,13 @@
+﻿namespace jokes_app.Models
+{
+    public class Joke
+    {
+        public int Id { get; set; }
+        public String JokeQuestion { get; set; }
+        public String JokeAnswer { get; set; }
+        public Joke()
+        {
+            
+        }
+    }
+}
